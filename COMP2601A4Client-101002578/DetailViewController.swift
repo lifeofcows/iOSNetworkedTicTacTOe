eca4d2fd3e7b822f7451ad4f7671926ca68a59a7
Maxim Kuzmenko <maximkuzmenko@maxims-macbook-pro.local>
2017-03-26 22:27:01 -0400

    Initial Commit

---
+++ COMP2601A4Client-101002578/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  COMP2601A4Client-101002578
+//
+//  Created by Maxim Kuzmenko on 2017-03-26.
+//  Copyright © 2017 Maxim Kuzmenko. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+
+}
+
---
+++ COMP2601A4Client-101002578Tests/COMP2601A4Client_101002578Tests.swift
@@ -0,0 +1,36 @@
+//
+//  COMP2601A4Client_101002578Tests.swift
+//  COMP2601A4Client-101002578Tests
+//
+//  Created by Maxim Kuzmenko on 2017-03-26.
+//  Copyright © 2017 Maxim Kuzmenko. All rights reserved.
+//
+
+import XCTest
+@testable import COMP2601A4Client_101002578
+
+class COMP2601A4Client_101002578Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
